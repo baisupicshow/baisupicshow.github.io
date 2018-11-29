62ce4865e48605b530109742443b80bfe7bb7742
baisu22 <1431077852@qq.com>
2018-11-29 22:17:33 +0800

    Update password.js

--- file/password.js
+++ file/password.js
@@ -1,7 +1,3 @@
-var password="";password=prompt('Do you love me? (Input yes or no):','');
-if (password != 'yes')   {alert("sorry to bother you");
-window.opener=null; window.close();}  // 密码不正确就关闭
-
-var password="";password=prompt('what is your name?(Input your name in Chinese)','');
+var password="";password=prompt('你知道我喜欢谁吗？^_^ (Input your name in Chinese)','');
 if (password != '胡思思')   {alert("sorry to bother you");
 window.opener=null; window.close();}
