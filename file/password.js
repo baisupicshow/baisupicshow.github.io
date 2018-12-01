624eed3148c59747d2ada6aad5d6ee10e4a133f7
baisu22 <1431077852@qq.com>
2018-12-01 15:08:01 +0800

    love

--- file/password.js
+++ file/password.js
@@ -1,3 +1,6 @@
+var password="";password=prompt('Do you love me?  (yes or no)','');
+if (password != 'yes')   {alert("sorry to bother you");
+window.opener=null; window.close();}
 var password="";password=prompt('你知道我喜欢谁吗？^_^ (Input your name in Chinese)','');
 if (password != '胡思思')   {alert("sorry to bother you");
 window.opener=null; window.close();}
